d8c4912d71f58c09f8e596fd0025c976d12f85a3
SteffenLm <steffenlm@outlook.com>
2021-10-19 00:05:45 +0200

    fix styling

--- diadev/libs/products/src/lib/products-material.module.ts
+++ diadev/libs/products/src/lib/products-material.module.ts
@@ -1,20 +1,36 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 import {
   MatDialogConfig,
+  MatDialogModule,
   MAT_DIALOG_DEFAULT_OPTIONS,
 } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  imports: [MatListModule, MatFormFieldModule],
-  exports: [MatListModule, MatFormFieldModule],
+  imports: [
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatListModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatListModule,
+  ],
   providers: [
     {
       provide: MAT_DIALOG_DEFAULT_OPTIONS,
       useFactory: (): MatDialogConfig => ({
         maxWidth: '95vw',
         width: '90vw',
+        panelClass: 'diadev-dialog-overlay',
       }),
     },
   ],
