d9e50c05eec31d6a783673ba82cd23820b2099bf
SteffenLm <steffenlm@outlook.com>
2021-10-31 12:20:30 +0100

    fix routermodule bug

--- diadev/libs/ui/src/lib/ui.module.ts
+++ diadev/libs/ui/src/lib/ui.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { UiMaterialModule } from './ui.material.module';
 
 import { FabIconButtonComponent } from './fab-icon-button/fab-icon-button.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
-  imports: [CommonModule, UiMaterialModule],
+  imports: [CommonModule, UiMaterialModule, RouterModule],
   declarations: [FabIconButtonComponent, ToolbarComponent],
   exports: [FabIconButtonComponent, ToolbarComponent],
 })
