5983cf4d49ad302053edc0830b185bc38c822aab
SteffenLm <steffenlm@outlook.com>
2021-12-08 21:21:48 +0100

    add injection token for products routing feature

--- diadev/apps/carbotracker/src/app/app-routing.module.ts
+++ diadev/apps/carbotracker/src/app/app-routing.module.ts
@@ -1,29 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PRODUCTS_ROUTING_PATH } from '@diadev/products';
+
+enum AppPath {
+  products = 'products',
+  currentMeal = 'current-meal',
+}
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: 'products',
+    redirectTo: AppPath.products,
   },
   {
-    path: 'products',
+    path: AppPath.products,
     loadChildren: () =>
       import('@diadev/products').then((m) => m.ProductsModule),
   },
   {
-    path: 'current-meal',
+    path: AppPath.currentMeal,
     loadChildren: () => import('@diadev/meals').then((m) => m.MealsModule),
   },
   {
     path: '**',
-    redirectTo: 'products',
+    redirectTo: AppPath.products,
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+  providers: [
+    {
+      provide: PRODUCTS_ROUTING_PATH,
+      useValue: AppPath.products,
+    },
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
