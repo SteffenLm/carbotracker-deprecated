7079af5afdfb3820896805031b30ab8e5ab98552
SteffenLm <steffenlm@outlook.com>
2021-12-13 18:48:47 +0100

    configure angular material globally

--- diadev/apps/carbotracker/src/app/app-material.module.ts
+++ diadev/apps/carbotracker/src/app/app-material.module.ts
@@ -5,6 +5,14 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import {
+  MAT_FORM_FIELD_DEFAULT_OPTIONS,
+  MatFormFieldDefaultOptions,
+} from '@angular/material/form-field';
+import {
+  MAT_SNACK_BAR_DEFAULT_OPTIONS,
+  MatSnackBarConfig,
+} from '@angular/material/snack-bar';
 
 @NgModule({
   imports: [
@@ -20,6 +28,22 @@ import { MatButtonModule } from '@angular/material/button';
     MatListModule,
     MatSidenavModule,
     MatToolbarModule,
+  ],
+  providers: [
+    {
+      provide: MAT_SNACK_BAR_DEFAULT_OPTIONS,
+      useFactory: (): MatSnackBarConfig => ({
+        duration: 2000,
+        horizontalPosition: 'center',
+        verticalPosition: 'bottom',
+      }),
+    },
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useFactory: (): MatFormFieldDefaultOptions => ({
+        appearance: 'outline',
+      }),
+    },
   ],
 })
 export class AppMaterialModule {}
