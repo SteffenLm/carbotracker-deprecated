3ba0b616ced4bd5eb8ea049c4b16a897c34a6933
SteffenLm <steffenlm@outlook.com>
2021-12-19 15:49:47 +0100

    fix exports

--- diadev/libs/products/src/index.ts
+++ diadev/libs/products/src/index.ts
@@ -1,5 +1,6 @@
 export * from './lib/+state/products/actions';
-export * from './lib/+state/products/products-entity.model';
+export * from './lib/model/products-entity.model';
 export * from './lib/+state/products/selectors';
 export * from './lib/products.module';
+export * from './lib/products-state.module';
 export { PRODUCTS_ROUTING_PATH } from './lib/model/products-routing.model';
