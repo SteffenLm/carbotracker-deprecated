ab21861cb0a621a6bb316bd067be88a31b715308
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2022-01-16 15:10:42 +0100

    test meals reducer

--- diadev/libs/meals/src/lib/+state/meals/model/meals-state.model.ts
+++ diadev/libs/meals/src/lib/+state/meals/model/meals-state.model.ts
@@ -1,5 +1,5 @@
 import { EntityState } from '@ngrx/entity';
-import { MealEntry } from '../../../model/meal-entry.models';
+import { getPastaMealEntry, MealEntry } from '../../../model/meal-entry.models';
 
 export const MEALS_FEATURE_KEY = 'meals';
 
@@ -16,3 +16,31 @@ export interface MealsState {
 export interface MealsPartialState {
   readonly [MEALS_FEATURE_KEY]: MealsState;
 }
+
+export const getEmptyMealsState = (): MealsState => ({
+  loaded: false,
+  error: null,
+  currentMeal: {
+    error: null,
+    mealEntries: {
+      ids: [],
+      entities: {},
+    },
+    selectedMealEntry: null,
+  },
+});
+
+export const getPastaSampleMealsState = (): MealsState => ({
+  loaded: false,
+  error: null,
+  currentMeal: {
+    error: 'Unknown Error',
+    mealEntries: {
+      ids: [getPastaMealEntry().id],
+      entities: {
+        [getPastaMealEntry().id]: getPastaMealEntry(),
+      },
+    },
+    selectedMealEntry: getPastaMealEntry().id,
+  },
+});
